a325e57299018fc2710278a2c7aa77e2fbfce67f
pduong02 <pduong02@gmail.com>
2023-11-20 15:04:21 -0500

    added more fingerprinting metrics

--- pixels_adscripts_tracker.py
+++ pixels_adscripts_tracker.py
@@ -1,29 +1,27 @@
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
+from concurrent.futures import ThreadPoolExecutor
 
 def count_tracking_pixels_and_trackers(url):
     try:
-        # Send an HTTP GET request to the URL
-        response = requests.get(url)
-        response.raise_for_status()
+        with requests.get(url) as response:
+            response.raise_for_status()
+            soup = BeautifulSoup(response.content, 'html.parser')
 
-        # Parse the HTML content using BeautifulSoup
-        soup = BeautifulSoup(response.content, 'html.parser')
+            # Count the number of tracking pixels (1x1 images)
+            tracking_pixels = soup.find_all('img', {'src': True, 'width': ['0', '1'], 'height': ['0', '1']})
+            num_tracking_pixels = len(tracking_pixels)
 
-        # Count the number of tracking pixels (1x1 images)
-        tracking_pixels = soup.find_all('img', {'src': True, 'width': ['0', '1'], 'height': ['0', '1']})
-        num_tracking_pixels = len(tracking_pixels)
+            # Identify potential ad trackers based on script URLs
+            ad_trackers = soup.find_all('script', src=True)
+            ad_trackers_filtered = [script for script in ad_trackers if is_ad_tracker(script['src'])]
+            num_ad_trackers = len(ad_trackers_filtered)
 
-        # Identify potential ad trackers based on script URLs
-        ad_trackers = soup.find_all('script', src=True)
-        ad_trackers_filtered = [script for script in ad_trackers if is_ad_tracker(script['src'])]
-        num_ad_trackers = len(ad_trackers_filtered)
+            # Detect fingerprinting indicators
+            fingerprint_indicators = detect_fingerprinting(soup)
 
-        # Detect fingerprinting indicators
-        fingerprint_indicators = detect_fingerprinting(url)
-
-        return num_tracking_pixels, num_ad_trackers, fingerprint_indicators
+            return num_tracking_pixels, num_ad_trackers, fingerprint_indicators
 
     except Exception as e:
         print(f"Error: {e}")
@@ -32,53 +30,54 @@ def count_tracking_pixels_and_trackers(url):
 def is_ad_tracker(script_url):
     # Add conditions here to filter out non-ad tracker scripts
     ad_keywords = ['ad', 'doubleclick', 'google-analytics', 'analytics.js']
-
-    # Check if any of the keywords are present in the script URL
     return any(keyword in script_url.lower() for keyword in ad_keywords)
 
-def detect_fingerprinting(url):
-    try:
-        # Send an HTTP GET request to the URL
-        response = requests.get(url)
-        response.raise_for_status()
+def detect_fingerprinting(soup):
+    fingerprint_indicators = []
 
-        # Parse the HTML content using BeautifulSoup
-        soup = BeautifulSoup(response.content, 'html.parser')
+    # Check for the presence of browser properties that can contribute to fingerprinting
+    user_agent_meta = soup.find('meta', {'name': 'useragent'})
+    if user_agent_meta:
+        fingerprint_indicators.append('User Agent Meta Tag')
 
-        # Check for potential fingerprinting indicators in the HTML
-        fingerprint_indicators = []
+    # Check for canvas fingerprinting
+    canvas_element = soup.find('canvas')
+    if canvas_element:
+        fingerprint_indicators.append('Canvas Fingerprinting')
 
-        # Check for the presence of browser properties that can contribute to fingerprinting
-        user_agent_meta = soup.find('meta', {'name': 'useragent'})
-        if user_agent_meta:
-            fingerprint_indicators.append('User Agent Meta Tag')
+    # Check for WebGL fingerprinting
+    webgl_element = soup.find('script', {'type': 'application/javascript'}, text=lambda t: 'WEBGL_debug_renderer_info' in t)
+    if webgl_element:
+        fingerprint_indicators.append('WebGL Fingerprinting')
 
-        # You may need to add more checks based on specific fingerprinting methods
+    # Check for font fingerprinting
+    font_element = soup.find('style', {'type': 'text/css'}, text=lambda t: '@font-face' in t)
+    if font_element:
+        fingerprint_indicators.append('Font Fingerprinting')
 
-        return fingerprint_indicators
+    # You may need to add more checks based on specific fingerprinting methods
 
-    except Exception as e:
-        print(f"Error: {e}")
-        return None
+    return fingerprint_indicators
+
+def analyze_website(url):
+    tracking_pixels, ad_trackers, fingerprint_indicators = count_tracking_pixels_and_trackers(url)
+    return {'Tracking Pixels': tracking_pixels, 'Ad Trackers': ad_trackers, 'Fingerprint Indicators': fingerprint_indicators}
 
 def analyze_websites_from_csv(csv_path):
     # Read the CSV into a DataFrame
     df = pd.read_csv(csv_path)
 
-    # Create columns to store the results
-    df['Tracking Pixels'] = None
-    df['Ad Trackers'] = None
-    df['Fingerprint Indicators'] = None
+    # Prepend 'https://' to the 'Domain Name' column
+    df['URL'] = 'https://' + df['Domain Name']
 
-    # Iterate through the DataFrame and analyze each website
-    for index, row in df.iterrows():
-        url = row['Domain Name']
-        tracking_pixels, ad_trackers, fingerprint_indicators = count_tracking_pixels_and_trackers(url)
-        df.at[index, 'Tracking Pixels'] = tracking_pixels
-        df.at[index, 'Ad Trackers'] = ad_trackers
-        df.at[index, 'Fingerprint Indicators'] = fingerprint_indicators
+    # Use ThreadPoolExecutor for parallelization
+    with ThreadPoolExecutor(max_workers=5) as executor:
+        results = list(executor.map(analyze_website, df['URL']))
 
-    return df
+    # Merge the results back into the DataFrame
+    result_df = pd.concat([df, pd.DataFrame(results)], axis=1)
+
+    return result_df
 
 # Example usage with a CSV file containing a 'Domain Name' column
 csv_file_path = 'df_1_copy.csv'
