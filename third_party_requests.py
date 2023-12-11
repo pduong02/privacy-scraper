6e6f7229b135181c76f5e0752380241bc28be6fa
vinayvangala2 <84420744+vinayvangala2@users.noreply.github.com>
2023-12-11 14:12:24 -0500

    Add files via upload

--- third_party_requests.py
+++ third_party_requests.py
@@ -1,17 +1,13 @@
 import requests
 from bs4 import BeautifulSoup
+import pandas as pd
 
 def count_third_party_requests(url):
     response = requests.get(url)
-
     if response.status_code == 200:
         soup = BeautifulSoup(response.content, 'html.parser')
         tpscripts = soup.find_all('script', src = True)
-        print(f"Number of third-party requests: {len(tpscripts)}")
-        #for resource in tpscripts:
-         #   print(resource['src'] if 'src' in resource.attrs else resource['href'])
+        print(f"{len(tpscripts)}")
     else:
-        print(f"Failed: {response.status_code}")
+        print(f"Failed: " + " " + url + " with " + {response.status_code})
 
-url = 'https://nba.com'
-count_third_party_requests(url)
